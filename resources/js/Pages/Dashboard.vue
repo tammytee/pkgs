26a5f1f4241bbc8b739d2154c5c8b344d7948040
Tammy Robinson <8689444+tammytee@users.noreply.github.com>
2023-03-31 21:02:31 -0400

    add cta to sync existing shipments

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import TrackerInput from '@/components/Tracker/TrackerInput.vue';
+import PrimaryButton from '@/components/PrimaryButton.vue';
 import TrackerTable from '@/components/Tracker/TrackerTable.vue';
+import { ArrowPathIcon } from '@heroicons/vue/24/outline';
 import { Head } from '@inertiajs/vue3';
 
 defineProps({ packages: Array });
@@ -17,6 +18,13 @@ defineProps({ packages: Array });
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="py-8">
+                    <PrimaryButton class="py-3 inline-flex items-center gap-x-1.5">
+                        <ArrowPathIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+                        Sync Shipments
+                    </PrimaryButton>
+                </div>
+
                 <TrackerTable :packages="packages" />
             </div>
         </div>
